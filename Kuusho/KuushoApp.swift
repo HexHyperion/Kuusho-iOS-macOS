be61be1d62963b6991a8fc8e9a62ea5a339afa9b
HexHyperion <szymonjan@gmail.com>
2025-12-17 13:43:18 +0100

    Initial Commit

---
+++ Kuusho/KuushoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuushoApp.swift
+//  Kuusho
+//
+//  Created by Szymon Urbaniak on 17/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct KuushoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
